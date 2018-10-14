39ec8292c9941d849890d23658758bc138fd03eb
sborquez <sebastian.borquez@gmail.com>
2018-10-13 23:51:06 -0300

    + Se termina vista del mapa + Se agregan pines para marcar lugares visitados + Se agrega detalles del lugar de cada pin

---
+++ collectibles/templatetags/transform_y.py
@@ -0,0 +1,12 @@
+from django import template
+from ..models import Location
+
+register = template.Library()
+
+
+@register.filter
+def transform_y(value):
+    try:
+        return int(100 * int(value) / int(Location.MAX_X))
+    except (ValueError, ZeroDivisionError):
+        return None
